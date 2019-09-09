cbb66041a0432d168df35d5e6a9bb587802af3dc
Wachirawit Onchet <ingwachi@gmail.com>
2019-09-09 12:34:58 +0700

    add atm annotation

--- src/main/java/atmannotation/Main.java
+++ src/main/java/atmannotation/Main.java
@@ -1,4 +1,4 @@
-package atm;
+package atmannotation;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -6,8 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("config.xml");
-        ATMSimulator atmSimulator = context.getBean("atmSimulator", ATMSimulator.class);
+                new ClassPathXmlApplicationContext("config-annotation.xml");
+        ATMSimulator atmSimulator = context.getBean(ATMSimulator.class);
         atmSimulator.run();
 
     }
